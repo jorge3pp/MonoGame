6bdc8f1817c4d760f3d3b7738ff497f2872f64da
Jorge Poveda Pérez <jorge23cs@gmail.com>
2018-09-20 16:01:19 +0100

    PErfect cube

--- 1W/1W/1W/Game1.cs
+++ 1W/1W/1W/Game1.cs
@@ -81,7 +81,8 @@ namespace _1W
             //setUpTexturedVertices();
             //setUpNormalVertices();
             //setUpIndexedColorVertices();
-            setUpPyramid();
+            //setUpPyramid();
+            setUpCube();
         }
 
         void setUpNormalVertices()
@@ -209,22 +210,21 @@ namespace _1W
             effect.TextureEnabled = false;
         }
 
-
         void setUpPyramid()
         {
             colorVertices = new VertexPositionColor[4];
 
             //Centre
-            colorVertices[0] = new VertexPositionColor(new Vector3(0, 0, 0), Color.Red);
+            colorVertices[0] = new VertexPositionColor(new Vector3(0, 1, 0), Color.Red);
 
-            //Top
-            colorVertices[1] = new VertexPositionColor(new Vector3(0, 1, 0), Color.Green);
+            //Base1
+            colorVertices[1] = new VertexPositionColor(new Vector3(-1, -1, -1), Color.Green);
 
-            //BR- BOTTOM RIGHT
-            colorVertices[2] = new VertexPositionColor(new Vector3(1, -1, 0), Color.Blue);
+            //B2
+            colorVertices[2] = new VertexPositionColor(new Vector3(1, -1, -1), Color.Blue);
 
-            //BL
-            colorVertices[3] = new VertexPositionColor(new Vector3(-1, -1, 0), Color.Black);
+            //B3
+            colorVertices[3] = new VertexPositionColor(new Vector3(0, -1, 1), Color.Black);
 
             indices = new short[9];
 
@@ -253,6 +253,126 @@ namespace _1W
             effect = new BasicEffect(GraphicsDevice);
             effect.VertexColorEnabled = true;
             effect.TextureEnabled = false;
+
+            effect.World *= Matrix.CreateRotationX(MathHelper.ToRadians(90));
+
+            /*See trough the model and show all vertices
+            RasterizerState state = new RasterizerState();
+            state.FillMode = FillMode.WireFrame;
+            state.CullMode = CullMode.None;
+            GraphicsDevice.RasterizerState = state;
+        */}
+
+        void setUpCube()
+        {
+            colorVertices = new VertexPositionColor[8];
+
+            //1Top antihorario
+            colorVertices[0] = new VertexPositionColor(new Vector3(1, 1, 1), Color.Red);
+
+            //2T AH
+            colorVertices[1] = new VertexPositionColor(new Vector3(-1, 1, 1), Color.Green);
+
+            //3T AH
+            colorVertices[2] = new VertexPositionColor(new Vector3(-1, 1, -1), Color.Blue);
+
+            //4T AH
+            colorVertices[3] = new VertexPositionColor(new Vector3(1, 1, -1), Color.Black);
+
+
+            //1Bottom antihorario
+            colorVertices[4] = new VertexPositionColor(new Vector3(1, -1, 1), Color.Pink);
+
+            //2B AH
+            colorVertices[5] = new VertexPositionColor(new Vector3(-1, -1, 1), Color.White);
+
+            //3B AH
+            colorVertices[6] = new VertexPositionColor(new Vector3(-1, -1, -1), Color.Yellow);
+
+            //4B AH
+            colorVertices[7] = new VertexPositionColor(new Vector3(1, -1, -1), Color.Black);
+
+            indices = new short[36];
+
+            //Triangle OneF1
+            indices[0] = 0;
+            indices[1] = 5;
+            indices[2] = 1;
+
+            //Triangle TwoF1
+            indices[3] = 0;
+            indices[4] = 4;
+            indices[5] = 5;
+
+            //Triangle ThreeF2
+            indices[6] = 1;
+            indices[7] = 5;
+            indices[8] = 2;
+
+
+            //Triangle FourF2
+            indices[9] = 2;
+            indices[10] = 5;
+            indices[11] = 6;
+
+            //Triangle FiveF3
+            indices[12] = 2;
+            indices[13] = 7;
+            indices[14] = 3;
+            
+            //Triangle SixF3
+            indices[15] = 2;
+            indices[16] = 6;
+            indices[17] = 7;
+
+            //Triangle SevenF4
+            indices[18] = 3;
+            indices[19] = 7;
+            indices[20] = 0;
+
+            //Triangle EightF4
+            indices[21] = 0;
+            indices[22] = 7;
+            indices[23] = 4;
+
+            //Triangle NineF5
+            indices[24] = 0;
+            indices[25] = 1;
+            indices[26] = 2;
+
+            //Triangle TenF5
+            indices[27] = 0;
+            indices[28] = 2;
+            indices[29] = 3;
+
+            //Triangle ElevenF6
+            indices[30] = 5;
+            indices[31] = 7;
+            indices[32] = 6;
+
+            //Triangle TwelveF6
+            indices[33] = 4;
+            indices[34] = 7;
+            indices[35] = 5;
+
+            vbuffer = new VertexBuffer(GraphicsDevice, VertexPositionColor.VertexDeclaration, colorVertices.Length, BufferUsage.WriteOnly);
+            vbuffer.SetData<VertexPositionColor>(colorVertices);
+
+            ibuffer = new IndexBuffer(GraphicsDevice, IndexElementSize.SixteenBits, indices.Length, BufferUsage.WriteOnly);
+            ibuffer.SetData<short>(indices);
+
+            effect = new BasicEffect(GraphicsDevice);
+            effect.VertexColorEnabled = true;
+            effect.TextureEnabled = false;
+
+
+            /*
+            //See trough the model and show all vertices
+            RasterizerState state = new RasterizerState();
+            state.FillMode = FillMode.WireFrame;
+            state.CullMode = CullMode.None;
+            GraphicsDevice.RasterizerState = state;
+            */
         }
 
         /// <summary>
@@ -274,22 +394,24 @@ namespace _1W
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            
 
-            // TODO: Add your update logic here
-
-            world *= Matrix.CreateRotationZ(MathHelper.ToRadians(1f));
-            //world *= Matrix.CreateRotationY(MathHelper.ToRadians(1f));
-            //world *= Matrix.CreateRotationX(MathHelper.ToRadians(1f));
+            //world *= Matrix.CreateRotationZ(MathHelper.ToRadians(1f));
+            //world *= Matrix.CreateRotationY(MathHelper.ToRadians(0.5f));
+            //world *= Matrix.CreateRotationX(MathHelper.ToRadians(0.1f));
 
             base.Update(gameTime);
         }
-
+        /*
         //ISRT - Identity, Scaled, Rotation, Translation
         Matrix world = 
             Matrix.Identity *
             Matrix.CreateScale(2) *
             Matrix.CreateRotationX(MathHelper.ToRadians(45)) *
-            Matrix.CreateTranslation(new Vector3(0, 0, -10));
+            Matrix.CreateRotationY(MathHelper.ToRadians(45)) *
+            Matrix.CreateRotationZ(MathHelper.ToRadians(30)) *
+            Matrix.CreateTranslation(new Vector3(0, 0, -10));*/
+        Matrix world = Matrix.Identity;
 
         /// <summary>
         /// This is called when the game should draw itself.
@@ -303,15 +425,13 @@ namespace _1W
             effect.Projection = projection;
             effect.World = world;
 
-            /*
-            //Si aumentamos el valor, aumenta la velocidad a la que rota el objeto.
-            effect.World *= Matrix.CreateRotationY(MathHelper.ToRadians(1f));
-            effect.World *= Matrix.CreateRotationX(MathHelper.ToRadians(2f));
-            effect.World *= Matrix.CreateRotationZ(MathHelper.ToRadians(0.5f));
-            */
 
-            //RasterizerState state = new RasterizerState();
-            //GraphicsDevice.RasterizerState = state;
+            //Si aumentamos el valor, aumenta la velocidad a la que rota el objeto.
+            world *= Matrix.CreateRotationY(MathHelper.ToRadians(1f));
+            world *= Matrix.CreateRotationX(MathHelper.ToRadians(2f));
+            world *= Matrix.CreateRotationZ(MathHelper.ToRadians(0.5f));
+            
+
 
             //drawColorVertices();
             //drawTexturedVertices();
@@ -378,5 +498,6 @@ namespace _1W
                     indices, 0, indices.Length / 3);
             }
         }
+        
     }
 }
